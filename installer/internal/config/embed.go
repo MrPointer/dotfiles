e144414b705e3137cac90e4ea05ebe8a8109b255
Timor Gruber <timor.gruber@solaredge.com>
2025-03-29 01:49:43 +0300

    add a compatibility config that's embedded in the binary
    Changing the config file is much easier than changing code,
    and by embedding it, we allow a "batteries included" experience.

---
+++ installer/internal/config/embed.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"embed"
+	"fmt"
+)
+
+//go:embed compatibility.yaml
+var configFS embed.FS
+
+// GetRawEmbeddedCompatibilityConfig returns the raw content of an embedded file
+// This is useful for testing purposes
+func GetRawEmbeddedCompatibilityConfig() ([]byte, error) {
+	// Read the embedded compatibility config
+	data, err := configFS.ReadFile("compatibility.yaml")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read embedded compatibility config: %w", err)
+	}
+
+	return data, nil
+}
