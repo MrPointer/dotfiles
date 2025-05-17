211bf411b9618db00a26cf2699612cd3113f737f
Timor Gruber <timor.gruber@solaredge.com>
2025-05-17 23:52:30 +0300

    move brew's unit tests into the test package

--- installer/lib/brew/brew_test.go
+++ installer/lib/brew/brew_test.go
@@ -1,8 +1,9 @@
-package brew
+package brew_test
 
 import (
 	"testing"
 
+	"github.com/MrPointer/dotfiles/installer/lib/brew"
 	"github.com/MrPointer/dotfiles/installer/lib/compatibility"
 	"github.com/MrPointer/dotfiles/installer/utils/logger"
 )
@@ -14,20 +15,22 @@ func TestDetectBrewPathReturnsExpectedPath(t *testing.T) {
 		expectedPath  string
 		errorExpected bool
 	}{
-		{"darwin/arm64", &compatibility.SystemInfo{OSName: "darwin", Arch: "arm64"}, MacOSARMBrewPath, false},
-		{"darwin/amd64", &compatibility.SystemInfo{OSName: "darwin", Arch: "amd64"}, MacOSIntelBrewPath, false},
-		{"linux/amd64", &compatibility.SystemInfo{OSName: "linux", Arch: "amd64"}, LinuxBrewPath, false},
+		{"darwin/arm64", &compatibility.SystemInfo{OSName: "darwin", Arch: "arm64"}, brew.MacOSARMBrewPath, false},
+		{"darwin/amd64", &compatibility.SystemInfo{OSName: "darwin", Arch: "amd64"}, brew.MacOSIntelBrewPath, false},
+		{"linux/amd64", &compatibility.SystemInfo{OSName: "linux", Arch: "amd64"}, brew.LinuxBrewPath, false},
 		{"unsupported", &compatibility.SystemInfo{OSName: "plan9", Arch: "amd64"}, "", true},
 		{"no sysinfo", nil, "", true},
 	}
 	for _, tt := range tests {
 		current := tt
 		t.Run(current.name, func(t *testing.T) {
-			b := &brewInstaller{
-				logger:     logger.DefaultLogger,
-				systemInfo: current.sysInfo,
-				commander:  nil,
+			opts := brew.Options{
+				Logger:     logger.DefaultLogger,
+				SystemInfo: current.sysInfo,
+				Commander:  nil,
 			}
+
+			b := brew.NewBrewInstaller(opts)
 			got, err := b.DetectBrewPath()
 			if (err != nil) != current.errorExpected {
 				t.Fatalf("DetectBrewPath() error = %v, wantError %v", err, current.errorExpected)
@@ -40,17 +43,17 @@ func TestDetectBrewPathReturnsExpectedPath(t *testing.T) {
 }
 
 func TestNewBrewInstallerCreatesMultiUserImplementationWhenOptionIsEnabled(t *testing.T) {
-	opts := Options{
+	opts := brew.Options{
 		MultiUserSystem: true,
 		Logger:          logger.DefaultLogger,
 		SystemInfo:      &compatibility.SystemInfo{OSName: "linux", Arch: "amd64"},
 		Commander:       nil,
 	}
-	installer := NewBrewInstaller(opts)
+	installer := brew.NewBrewInstaller(opts)
 	if installer == nil {
 		t.Fatal("expected non-nil installer")
 	}
-	if _, ok := installer.(*MultiUserBrewInstaller); !ok {
+	if _, ok := installer.(*brew.MultiUserBrewInstaller); !ok {
 		t.Errorf("expected *MultiUserBrewInstaller, got %T", installer)
 	}
 }
