74ad427c6ab8c8cb77fa6a7b7b961b940940f586
Timor Gruber <timor.gruber@solaredge.com>
2025-05-30 19:44:26 +0300

    add os-manager interface to decouple code from raw os calls

---
+++ installer/utils/osmanager/os_manager.go
@@ -0,0 +1,155 @@
+package osmanager
+
+import (
+	"fmt"
+	"os/user"
+
+	"github.com/MrPointer/dotfiles/installer/utils"
+	"github.com/MrPointer/dotfiles/installer/utils/logger"
+)
+
+// UserManager defines operations for managing system users
+//
+//go:generate moq -out user_manager_moq.go . UserManager
+type UserManager interface {
+	// AddUser creates a new user in the system
+	AddUser(username string) error
+
+	// AddUserToGroup adds a user to a specified group
+	AddUserToGroup(username, group string) error
+
+	// UserExists checks if a user exists in the system
+	UserExists(username string) (bool, error)
+}
+
+// SudoManager defines operations for managing sudo permissions
+//
+//go:generate moq -out sudo_manager_moq.go . SudoManager
+type SudoManager interface {
+	// AddSudoAccess grants password-less sudo access to a user
+	AddSudoAccess(username string) error
+}
+
+// FilePermissionManager defines operations for managing filesystem permissions
+//
+//go:generate moq -out filesystem_manager_moq.go . FileSystemManager
+type FilePermissionManager interface {
+	// SetOwnership sets ownership of a directory to a user
+	SetOwnership(path, username string) error
+}
+
+// OsManager combines all system operation interfaces
+//
+//go:generate moq -out os_manager_moq.go . OsManager
+type OsManager interface {
+	UserManager
+	SudoManager
+	FilePermissionManager
+}
+
+// UnixOsManager implements OsManager for Unix-like systems
+type UnixOsManager struct {
+	logger    logger.Logger
+	commander utils.Commander
+	isRoot    bool
+}
+
+var _ OsManager = (*UnixOsManager)(nil)
+
+// NewUnixOsManager creates a new UnixOsManager
+func NewUnixOsManager(logger logger.Logger, commander utils.Commander, isRoot bool) *UnixOsManager {
+	return &UnixOsManager{
+		logger:    logger,
+		commander: commander,
+		isRoot:    isRoot,
+	}
+}
+
+// UserExists checks if a user exists in the system
+func (u *UnixOsManager) UserExists(username string) (bool, error) {
+	_, err := user.Lookup(username)
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
+}
+
+// AddUser creates a new user in the system
+func (u *UnixOsManager) AddUser(username string) error {
+	u.logger.Info("User '%s' does not exist, creating...", username)
+
+	// Try useradd, fallback to adduser
+	useraddCmd := []string{"useradd", "-m", "-s", "/bin/bash", username}
+	if !u.isRoot {
+		useraddCmd = append([]string{"sudo"}, useraddCmd...)
+	}
+
+	err := u.commander.Run(useraddCmd[0], useraddCmd[1:]...)
+	if err != nil {
+		// Try adduser as fallback
+		adduserCmd := []string{"adduser", "--disabled-password", "--gecos", "''", username}
+		if !u.isRoot {
+			adduserCmd = append([]string{"sudo"}, adduserCmd...)
+		}
+
+		err = u.commander.Run(adduserCmd[0], adduserCmd[1:]...)
+		if err != nil {
+			return fmt.Errorf("failed to create user '%s' with useradd/adduser: %w", username, err)
+		}
+	}
+
+	return nil
+}
+
+// AddUserToGroup adds a user to a specified group
+func (u *UnixOsManager) AddUserToGroup(username, group string) error {
+	u.logger.Info("Adding '%s' to %s group", username, group)
+	usermodCmd := []string{"usermod", "-aG", group, username}
+	if !u.isRoot {
+		usermodCmd = append([]string{"sudo"}, usermodCmd...)
+	}
+
+	err := u.commander.Run(usermodCmd[0], usermodCmd[1:]...)
+	// Often we don't care if the user is already in the group
+	if err != nil {
+		u.logger.Debug("Note: User might already be in the %s group", group)
+	}
+
+	return nil
+}
+
+// AddSudoAccess grants password-less sudo access to a user
+func (u *UnixOsManager) AddSudoAccess(username string) error {
+	sudoersFile := fmt.Sprintf("/etc/sudoers.d/%s", username)
+	sudoersLine := fmt.Sprintf("%s ALL=(ALL) NOPASSWD:ALL", username)
+
+	var sudoPrefix string
+	if !u.isRoot {
+		sudoPrefix = "sudo "
+	}
+
+	// Use shell to echo and tee the line into the sudoers file
+	shCmd := []string{"sh", "-c", fmt.Sprintf("echo '%s' | %stee %s", sudoersLine, sudoPrefix, sudoersFile)}
+	err := u.commander.Run(shCmd[0], shCmd[1:]...)
+	if err != nil {
+		return fmt.Errorf("failed to add passwordless sudo for '%s': %w", username, err)
+	}
+
+	return nil
+}
+
+// SetOwnership sets ownership of a directory to a user
+func (u *UnixOsManager) SetOwnership(path, username string) error {
+	u.logger.Info("Setting ownership of %s to %s", path, username)
+	chownCmd := []string{"chown", "-R", fmt.Sprintf("%s:%s", username, username), path}
+	if !u.isRoot {
+		chownCmd = append([]string{"sudo"}, chownCmd...)
+	}
+
+	err := u.commander.Run(chownCmd[0], chownCmd[1:]...)
+	if err != nil {
+		return fmt.Errorf("failed to chown %s: %w", path, err)
+	}
+
+	return nil
+}
