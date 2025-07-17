f5778a6b44265b93840968ddda94cc20fbb0a2b2
Timor Gruber <timor.gruber@solaredge.com>
2025-07-17 21:13:50 +0100

    add dotfiles data initializer

---
+++ installer/lib/dotfilesmanager/data.go
@@ -0,0 +1,30 @@
+package dotfilesmanager
+
+import (
+	"github.com/samber/mo"
+)
+
+type DotfilesData struct {
+	Email         string                         `mapstructure:"email"`
+	FirstName     string                         `mapstructure:"first_name"`
+	LastName      string                         `mapstructure:"last_name"`
+	GpgSigningKey mo.Option[string]              `mapstructure:"gpg_signing_key"`
+	WorkEnv       mo.Option[DotfilesWorkEnvData] `mapstructure:"work_env"`
+	SystemData    mo.Option[DotfilesSystemData]  `mapstructure:"system_data"`
+}
+
+type DotfilesWorkEnvData struct {
+	WorkName  string `mapstructure:"work_name"`
+	WorkEmail string `mapstructure:"work_email"`
+}
+
+type DotfilesSystemData struct {
+	Shell           string `mapstructure:"shell"`
+	User            string `mapstructure:"user"`
+	MultiUserSystem bool   `mapstructure:"multi_user_system"`
+	BrewUser        bool   `mapstructure:"brew_user"`
+}
+
+type DotfilesDataInitializer interface {
+	Initialize(data DotfilesData) error
+}
