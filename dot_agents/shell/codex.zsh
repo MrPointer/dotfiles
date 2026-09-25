59be079ce89dec7ca1b27df44225b8b7790561f0
Timor Gruber <timor.gruber@gmail.com>
2026-09-25 21:10:53 +0300

    add support for running acp agents with nono
    Unlike regular agents, it requires special wrapping, each runtime
    and its own acp support - It's embedded in opencode, but comes
    as external binaries for codex and claude.
    On top of that, a single "binary" managed with these dotfiles,
    under `.local/bin`, allows hosts such as the Zed IDE to quickly
    make use of those wrappers and nono without running into pathing issues,
    as the shell functions used in the terminal are normally used
    within an interactive zsh session.

--- dot_agents/shell/codex.zsh
+++ dot_agents/shell/codex.zsh
@@ -7,3 +7,17 @@ function codex() {
     "${CODEX_SSH_SIGNING_KEY:-$HOME/.ssh/id_ed25519}" \
     "$@"
 }
+
+function codex-acp() {
+  local NONO_AGENT_ACP=1
+  local -x INITIAL_AGENT_MODE=agent-full-access
+
+  _nono_agent_run \
+    my-codex \
+    "$HOME/.config/opencode/gitconfig" \
+    npx \
+    "${CODEX_SSH_SIGNING_PUBLIC_KEY:-$HOME/.ssh/id_ed25519.pub}" \
+    "${CODEX_SSH_SIGNING_KEY:-$HOME/.ssh/id_ed25519}" \
+    -y @agentclientprotocol/codex-acp@latest \
+    "$@"
+}
